5d82bbdc74234cd13557fe412e9611a1ed92945c
jcadic <j.cadic@9dw-lab.jp>
2019-01-08 11:32:19 +0900

    Wav2Letter

---
+++ wav2letter.py
@@ -0,0 +1,28 @@
+import argparse
+from iyo.core.nn.modules.wav2letter import Wav2Letter
+import numpy as np
+import os
+
+if __name__ == "__main__":
+    # Load the args
+    parser = argparse.ArgumentParser(description='Wav2Letter')
+    parser.add_argument('--batch-size', type=int, default=56)
+    parser.add_argument('--epochs', type=int, default=1000)
+    parser.add_argument("--npy_dir", type=str, default="/mnt/IYOCorpora/YKH/JSUT")
+    parser.add_argument("--checkpoint", type=str, default="/home/jcadic/NHDModels/YKH/JSUT/Wav2Letter/wav2letter_jsut.pth")
+    args = parser.parse_args()
+
+    # Create the model
+    model = Wav2Letter(num_classes=len("{}/index2char.npy".format(args.npy_dir)),
+                       index2char="{}/index2char.npy".format(args.npy_dir),
+                       trace_file="/tmp/Wav2Letter_{}.txt".format(os.getpid()))
+
+    # Fit the model
+    model.fit(x_train=np.load("{}/x_train.npy".format(args.npy_dir)),
+              y_train=np.load("{}/y_train.npy".format(args.npy_dir)),
+              x_dev=np.load("{}/x_dev.npy".format(args.npy_dir)),
+              y_dev=np.load("{}/y_dev.npy".format(args.npy_dir)),
+              batch_size=args.batch_size,
+              epochs=args.epochs,
+              checkpoint=args.checkpoint)
+
